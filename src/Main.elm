4f82e4d44bfab329bcd468fcd9c01b0770e916f7
Girish S <girish.sonawane@gmail.com>
2016-10-22 18:47:41 +0530

    initial game working

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,9 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Html.App exposing (beginnerProgram)
+import Matrix exposing (..)
+import Matrix.Extra exposing (..)
+import Array
 
 
 -- # Main
@@ -12,7 +15,7 @@ import Html.App exposing (beginnerProgram)
 main : Program Never
 main =
     beginnerProgram
-        { model = init
+        { model = initBar
         , view = view
         , update = update
         }
@@ -23,19 +26,50 @@ main =
 
 
 type alias Model =
-    {isOn: List Bool}
+    { isOn : Matrix Bool }
+
 
 
 --init : Model
+
+
 init =
-    {isOn = [True, True, True]}
+    { isOn = Matrix.repeat 6 5 True }
+
+
+initBar =
+    let
+        x =
+            Matrix.indexedMap
+                (\x y value ->
+                    if x == 0 || x == 2 then
+                        False
+                    else
+                        True
+                )
+                init.isOn
+    in
+        { isOn = x }
+
+
+isSolved : Model -> Bool
+isSolved model =
+    model.isOn
+        |> Matrix.filter identity
+        |> Array.isEmpty
+
 
 
 -- # Messages
 
 
 type Msg
-    = Toggle Int
+    = Toggle LightIndex
+    | Restart
+
+
+type alias LightIndex =
+    { x : Int, y : Int }
 
 
 
@@ -45,50 +79,116 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Toggle ix ->
-            {model | isOn = toggleLights ix model.isOn}
+        Toggle index ->
+            { model | isOn = toggleLights index model.isOn }
+
+        Restart ->
+            init
 
 
-toggleLights : Int -> List Bool -> List Bool
-toggleLights ix list =
-    List.indexedMap
-        (\i isOn ->
-            if ix == i then
-                not isOn
-            else if ix == i + 1 then
-                not isOn
-            else if ix == i - 1 then
-                not isOn
-            else
-                isOn
-        ) list
+toggleLights : LightIndex -> Matrix Bool -> Matrix Bool
+toggleLights index isOn =
+    Matrix.indexedMap
+        (\x y value ->
+            let
+                ns =
+                    Matrix.Extra.neighbours x y isOn
+
+                live_ns =
+                    List.foldr oneiftrue 0 ns
+
+                _ =
+                    Debug.log "live_ns" live_ns
+            in
+                if value then
+                    if live_ns == 2 || live_ns == 3 then
+                        True
+                    else if live_ns > 3 then
+                        False
+                    else if live_ns < 2 then
+                        False
+                    else
+                        False
+                else if live_ns == 3 then
+                    True
+                else
+                    False
+        )
+        isOn
+
+
+oneiftrue flag sum =
+    if flag then
+        sum + 1
+    else
+        sum
+
+
+
 -- # View
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-    [
-        model.isOn
-            |> List.indexedMap btn
+        [ model.isOn
+            |> Matrix.indexedMap btn
+            |> toDivs
+        , winScreen model
+        , hr [] []
+        , Html.button
+            [ onClick Restart ]
+            [ text "Restart" ]
+        , hr [] []
+        , div []
+            [ model.isOn
+                |> Matrix.Extra.prettyPrint
+            ]
+        ]
+
+
+winScreen : Model -> Html Msg
+winScreen model =
+    if isSolved model then
+        h1 []
+            [ text "Win!!" ]
+    else
+        div [] []
+
+
+toDivs : Matrix (Html Msg) -> Html Msg
+toDivs matrix =
+    let
+        row y =
+            Matrix.getRow y matrix
+                |> Maybe.map Array.toList
+                |> Maybe.withDefault []
+                |> div []
+
+        height =
+            Matrix.height matrix
+    in
+        [0..height]
+            |> List.map row
             |> div []
 
-    ]
 
-btn : Int -> Bool -> Html Msg
-btn ix isOn =
-    div [
-            style [
-            ("background-color",
-                if isOn then
+btn : Int -> Int -> Bool -> Html Msg
+btn ix iy isOn =
+    div
+        [ style
+            [ ( "background-color"
+              , if isOn then
                     "orange"
                 else
-                    "grey")
-            , ( "width", "40px" )
-            , ( "height", "40px" )
+                    "grey"
+              )
+            , ( "width", "80px" )
+            , ( "height", "80px" )
             , ( "margin", "2px" )
+            , ( "border-radius", "10px" )
             , ( "display", "inline-block" )
             ]
-            , onClick (Toggle ix)
+        , onClick (Toggle { x = ix, y = iy })
         ]
         []
